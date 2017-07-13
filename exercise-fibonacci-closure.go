a21e601785c41c053a4e4e0dd6b8b590b57b5f4a
tossmilestone <tossmilestone@gmail.com>
2017-07-13 17:12:29 +0800

    Improve the fibonacci algorithm

--- exercise-fibonacci-closure.go
+++ exercise-fibonacci-closure.go
@@ -5,9 +5,9 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	a, b, c := 0, 1, 0
+	a, b := 1, 0
 	return func() int {
-		c, a, b = a, b, a+b
+		a, b = b, a+b
 		return c
 	}
 }
